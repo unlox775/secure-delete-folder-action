4778aa7c6008c83ea9aa48ddad598f452181af5a
unlox775alto <dave.buchanan@alto.com>
2022-08-31 09:41:51 -0700

    Initial upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+mkdir -p $HOME/Library/Scripts/Folder\ Action\ Scripts
+rm -f $HOME/Library/Scripts/Folder\ Action\ Scripts/secure-delete.scpt 
+ln secure-delete.scpt $HOME/Library/Scripts/Folder\ Action\ Scripts/secure-delete.scpt 
+
+echo Now open up Folder Actions Setup, and attach whatever folder you want.
